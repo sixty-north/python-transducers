123e495d98104eeb25706ded8230cde558697ad6
Robert Smallshire <robert@smallshire.org.uk>
2015-03-27 20:46:57 +0100

    Fixes issue #3 using functools.wraps to preserve docstrings.

--- transducer/_util.py
+++ transducer/_util.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 
 #  A sentinel for indicating unset function arguments in places
 #  where None would be a legitimate value.
@@ -5,13 +7,19 @@ UNSET = object()
 
 
 def pending_in(queue):
+    """Yield items from the left of a queue.
+    """
     while queue:
         yield queue.popleft()
 
 
 def coroutine(func):
+    """Decorator for priming generator-based coroutines.
+    """
+    @wraps(func)
     def start(*args, **kwargs):
         g = func(*args, **kwargs)
         next(g)
         return g
+
     return start
