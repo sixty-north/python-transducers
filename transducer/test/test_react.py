f8d7eb0f2ee7b80bdbde2170b83dd5b2770009ae
Robert Smallshire <robert@smallshire.org.uk>
2015-03-30 20:53:45 +0200

    Improve test coverage of react.py by testing terminating transducers.

--- transducer/test/test_react.py
+++ transducer/test/test_react.py
@@ -1,26 +1,24 @@
 import unittest
 from transducer.functional import compose
 from transducer.react import transduce
-from transducer.sinks import CollectingSink
+from transducer.sinks import CollectingSink, SingularSink
 from transducer.sources import iterable_source
-from transducer.transducers import (mapping, pairwise, filtering)
+from transducer.transducers import (mapping, pairwise, filtering, first)
 
 
 class TestComposedTransducers(unittest.TestCase):
 
+    def test_early_terminating_transducer(self):
+        input = [0.0, 0.2, 0.8, 0.9, 1.1, 2.3, 2.6, 3.0, 4.1]
+        output = SingularSink()
+
+        iterable_source(iterable=input,
+                        target=transduce(first(lambda x: x > 1.0),
+                                         target=output()))
+        self.assertEqual(output.value, 1.1)
+
     def test_chained_transducers(self):
-
-
-        input = [0.0,
-                 0.2,
-                 0.8,
-                 0.9,
-                 1.1,
-                 2.3,
-                 2.6,
-                 3.0,
-                 4.1]
-
+        input = [0.0, 0.2, 0.8, 0.9, 1.1, 2.3, 2.6, 3.0, 4.1]
         output = CollectingSink()
 
         iterable_source(iterable=input,
@@ -31,7 +29,8 @@ class TestComposedTransducers(unittest.TestCase):
                                     mapping(lambda _: "double-click")),
                             target=output()))
 
-        pass
+        result = list(output)
+        self.assertListEqual(result, ['double-click', 'double-click', 'double-click', 'double-click', 'double-click'])
 
 
 if __name__ == '__main__':
