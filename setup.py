6dd990659e9fd13823344ce47567aa548b156ea2
Robert Smallshire <robert@smallshire.org.uk>
2017-06-28 22:43:21 +0200

    Updates supported Python versions.

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
